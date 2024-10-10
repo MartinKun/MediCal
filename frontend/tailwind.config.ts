5030beded9ff083859673f7bbe0adb3a8fcdae95
MartinKun <martinkunbrc@hotmail.com>
2024-10-10 18:36:09 -0300

    frontend: Animations were added to the tailwind.config.ts.

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -13,6 +13,37 @@ const config: Config = {
         foreground: "var(--foreground)",
       },
     },
+    keyframes: {
+      fadeIn: {
+        "0%": {
+          opacity: "0",
+        },
+        "100%": {
+          opacity: "1",
+        },
+      },
+      scaleUp: {
+        "0%": {
+          transform: "scale(0.95)",
+        },
+        "100%": {
+          transform: "scale(1)",
+        },
+      },
+      spin: {
+        "0%": {
+          transform: "rotate(0deg)",
+        },
+        "100%": {
+          transform: "rotate(360deg)",
+        },
+      },
+    },
+    animation: {
+      fadeIn: "fadeIn 0.5s ease-out both",
+      scaleUp: "scaleUp 0.5s ease-out both",
+      spin: "spin 1s linear infinite",
+    },
   },
   plugins: [],
 };
