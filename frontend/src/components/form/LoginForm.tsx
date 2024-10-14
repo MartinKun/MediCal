cae6549eb58304a1596496621ac0007f15c39c1d
MartinKun <martinkunbrc@hotmail.com>
2024-10-14 18:53:30 -0300

    frontend: login service was implemented in Login page.

--- frontend/src/components/form/LoginForm.tsx
+++ frontend/src/components/form/LoginForm.tsx
@@ -3,12 +3,39 @@ import { InputWithIcon } from "./InputWithIcon";
 import { loginInputFields } from "@/util/inputFields";
 import { SubmitButton } from "./SubmitButton";
 import useFormState from "@/hook/useForm";
+import { useBoundStore } from "@/store/store";
+import services from "@/services";
+import { useRouter } from "next/navigation";
 
 export const LoginForm = () => {
+  const showLoader = useBoundStore((state) => state.showLoader);
+  const hideLoader = useBoundStore((state) => state.hideLoader);
+  const setUser = useBoundStore((state) => state.setUser);
   const { formState, setFormState } = useFormState({ email: "", password: "" });
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      showLoader();
+      const response = await services.login(formState);
+      if (response) {
+        setUser({
+          userData: null,
+          token: response.data.token,
+        });
+        router.push("/myAppointments");
+      }
+    } catch (error) {
+      console.error("Login failed:", error);
+    } finally {
+      hideLoader();
+    }
+  };
 
   return (
-    <form className="space-y-6">
+    <form className="space-y-6" onSubmit={handleSubmit}>
       {loginInputFields.map((field, index) => (
         <InputWithIcon
           key={index}
