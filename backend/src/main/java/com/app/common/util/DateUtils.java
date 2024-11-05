c6f2095d22a8cdf2f86078fcaabde2ba43245208
MartinKun <martinkunbrc@hotmail.com>
2024-11-05 19:44:44 -0300

    backend: Add DateUtils class to format LocalDateTime

---
+++ backend/src/main/java/com/app/common/util/DateUtils.java
@@ -0,0 +1,18 @@
+package com.app.common.util;
+
+import com.app.exception.DateFormatException;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateUtils {
+
+    public static String formatDate(LocalDateTime date) throws DateFormatException {
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+            return date.format(formatter);
+        } catch (Exception e) {
+            throw new DateFormatException();
+        }
+    }
+}
