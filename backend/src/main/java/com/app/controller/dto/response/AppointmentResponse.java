ab2a91469afaf6684c441cba3b6fcd2d4bff9f5b
MartinKun <martinkunbrc@hotmail.com>
2024-11-05 19:52:24 -0300

    backend: Update AppointmentResponse values for better clarity and organization

--- backend/src/main/java/com/app/controller/dto/response/AppointmentResponse.java
+++ backend/src/main/java/com/app/controller/dto/response/AppointmentResponse.java
@@ -1,6 +1,5 @@
 package com.app.controller.dto.response;
 
-import com.app.persistence.entity.Patient;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -11,9 +10,18 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Builder
 public class AppointmentResponse {
-    private String userName;
-    private String userAvatar;
-    private LocalDateTime date;
+    private ParticipantInfo participant; // Data for the counterpart in the appointment (patient or doctor)
+    private String date;
     private String reason;
     private String address;
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class ParticipantInfo {
+        private String fullName;
+        private String avatar;
+    }
 }
