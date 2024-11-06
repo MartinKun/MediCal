51f34a1f4db7bf92e1989b52050db96116232c3a
MartinKun <martinkunbrc@hotmail.com>
2024-11-06 09:39:03 -0300

    backend: DateUtils was updated

--- backend/src/main/java/com/app/common/util/DateUtils.java
+++ backend/src/main/java/com/app/common/util/DateUtils.java
@@ -2,6 +2,7 @@ package com.app.common.util;
 
 import com.app.exception.DateFormatException;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -10,6 +11,15 @@ public class DateUtils {
     public static String formatDate(LocalDateTime date) throws DateFormatException {
         try {
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+            return date.format(formatter);
+        } catch (Exception e) {
+            throw new DateFormatException();
+        }
+    }
+
+    public static String formatDate(LocalDate date) throws DateFormatException {
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
             return date.format(formatter);
         } catch (Exception e) {
             throw new DateFormatException();
