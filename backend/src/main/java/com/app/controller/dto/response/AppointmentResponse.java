aed2e334ccb1866271e488795b3f6e6fb0d7815c
MartinKun <martinkunbrc@hotmail.com>
2024-11-08 13:04:13 -0300

    backend: Update AppointmentResponse to include appointment and participant IDs

--- backend/src/main/java/com/app/controller/dto/response/AppointmentResponse.java
+++ backend/src/main/java/com/app/controller/dto/response/AppointmentResponse.java
@@ -1,5 +1,6 @@
 package com.app.controller.dto.response;
 
+import com.app.controller.dto.enums.RoleEnum;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -10,10 +11,11 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Builder
 public class AppointmentResponse {
-    private ParticipantInfo participant; // Data for the counterpart in the appointment (patient or doctor)
-    private String date;
+    private Long id;
     private String reason;
     private String address;
+    private String date;
+    private ParticipantInfo participant; // Data for the counterpart in the appointment (patient or doctor)
 
     @Getter
     @Setter
@@ -21,6 +23,8 @@ public class AppointmentResponse {
     @AllArgsConstructor
     @Builder
     public static class ParticipantInfo {
+        private Long id;
+        private RoleEnum role;
         private String fullName;
         private String avatar;
     }
